e9c60995521bb00b38588c678bcafe4a2204e9b2
Oy vey <oyiveyi@gmail.com>
2025-04-19 00:08:17 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -15,30 +15,28 @@ public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
-//        var config = new Config();
-//        config.load("src/main/resources/application.properties");
-//        config.get("db.driver-class-name");
-//        try (var connection = DriverManager.getConnection(
-//                config.get("db.url"),
-//                config.get("db.username"),
-//                config.get("db.password"))) {
-//            var store = new JdbcStore(connection);
-//            var post = new Post();
-//            post.setTitle("Super Java Job");
-//            store.save(post);
-//            var scheduler = new SchedulerManager();
-//            scheduler.init();
-//            scheduler.load(
-//                    Integer.parseInt(config.get("rabbit.interval")),
-//                    SuperJobGrab.class,
-//                    store);
-//            Thread.sleep(100000);
-//        } catch (SQLException e) {
-//            LOG.error("When create a connection", e);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-        HabrCareerParse habrCareerParse = new HabrCareerParse();
-        habrCareerParse.fetch();
+        var config = new Config();
+        config.load("src/main/resources/application.properties");
+        config.get("db.driver-class-name");
+        try (var connection = DriverManager.getConnection(
+                config.get("db.url"),
+                config.get("db.username"),
+                config.get("db.password"))) {
+            var store = new JdbcStore(connection);
+            var post = new Post();
+            post.setTitle("Super Java Job");
+            store.save(post);
+            var scheduler = new SchedulerManager();
+            scheduler.init();
+            scheduler.load(
+                    Integer.parseInt(config.get("rabbit.interval")),
+                    SuperJobGrab.class,
+                    store);
+            Thread.sleep(100000);
+        } catch (SQLException e) {
+            LOG.error("When create a connection", e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
